2cadee8c5b06951681275819d5ce838ebba9fe45
elluis <elluis.codes@gmail.com>
2022-09-29 18:43:15 -0500

    added basic router functionality - through App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Error from "./pages/Error";
+
+function App () {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={ '/' } element={ <Home/> }></Route>
+                <Route path={ 'about' } element={ <About/> }></Route>
+                <Route path={ 'products' } element={ <Products/> }></Route>
+                <Route path={'*'} element={<Error/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
