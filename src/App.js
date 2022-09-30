b3a34e11b372f3463d4b4940643a2c181ab46e54
elluis <elluis.codes@gmail.com>
2022-09-29 22:14:57 -0500

    added initial useParams via SingleProduct.js -

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
-import Error from "./pages/Error";import SharedLayout from "./pages/SharedLayout";
+import Error from "./pages/Error";
+import SharedLayout from "./pages/SharedLayout";
+import SingleProduct from "./pages/SingleProduct";
 
 function App () {
     return (<BrowserRouter>
         {/* // ensure to keep external components outside of our <Routes/> */ }
-        {/* <Navbar/> */}
+        {/* <Navbar/> */ }
         <Routes>
             <Route path={ '/' } element={ <SharedLayout/> }>
-                <Route index element={<Home/>}/>
+                <Route index element={ <Home/> }/>
                 <Route path={ 'about' } element={ <About/> }/>
                 <Route path={ 'products' } element={ <Products/> }/>
+                <Route path={ 'products/:productId' } element={ <SingleProduct/> }/>
                 <Route path={ '*' } element={ <Error/> }/>
             </Route>
         </Routes>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,17 @@
+import {Link, useParams} from "react-router-dom";
+
+
 const SingleProduct = () => {
+    const {productId}= useParams();
+
+    console.log (productId);
+
+
+    // console.log (useParams());
   return (
     <section className='section product'>
-      <h2>single product</h2>
+        <h4>{productId}</h4>
+        <Link to={'/products'}>back to products</Link>
     </section>
   );
 };
